80825ea92494f4288b8344b775701a4e2c7a6925
Luis del Valle <luis.delvalle0124@gmail.com>
2022-09-20 22:46:23 -0700

    Passed 2 test, 4 more to go

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    if (a == b) return a;
+    else if (a < b) return a + sumAll(a + 1, b);
+    else console.log(a + sumAll(a - 1, b));
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
