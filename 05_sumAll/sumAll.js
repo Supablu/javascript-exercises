96200b7666cb119bdbdc4a5b826cbd3fde3709a7
Luis del Valle <luis.delvalle0124@gmail.com>
2022-09-21 11:44:19 -0700

    Tried something new, still unable to pass test after #2

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,15 @@
-const sumAll = function (a, b) {
-    if (a == b) return a;
-    else if (a < b) return a + sumAll(a + 1, b);
-    else console.log(a + sumAll(a - 1, b));
-};
+const sumAll = function (min, max) {
+    //     if (a == b) return a;
+    //     else if (a < b) return a + sumAll(a + 1, b);
+    //     else console.log(a + sumAll(a - 1, b));
+    // };
 
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
